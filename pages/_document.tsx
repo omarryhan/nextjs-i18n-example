4b47723a54ba6f582d6defb8ccb3b299e599dea7
Omar Ryhan <omarryhan@gmail.com>
2020-07-13 18:54:30 +0200

    _document.tsx now only sets an html lang property only if the header is defined in allLanguages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import Document, {
   DocumentInitialProps, DocumentProps, Html, Head, Main, NextScript,
 } from 'next/document';
-import i18nConfig, { Language } from '../i18n.config';
+import i18nConfig from '../i18n.config';
 
-const { allLanguages, defaultLanguage } = i18nConfig;
+const { allLanguages } = i18nConfig;
 
-const getCurrentLanguage = ({ __NEXT_DATA__ }: DocumentProps): Language => {
+const getCurrentLanguage = ({ __NEXT_DATA__ }: DocumentProps): string | undefined => {
   const { page } = __NEXT_DATA__;
   const [, langQuery] = page.split('/');
   const lang = Object.keys(allLanguages).find(
     (language) => allLanguages[language].prefix === langQuery,
   );
 
-  return allLanguages[lang || defaultLanguage.prefix] || defaultLanguage;
+  if (typeof lang === 'string') {
+    const language = allLanguages[lang];
+    if (language) {
+      return language.prefix;
+    }
+  }
+
+  return undefined;
 };
 
 export default class MyDocument extends Document<DocumentInitialProps> {
   render(): JSX.Element {
-    const { prefix } = getCurrentLanguage(this.props);
+    const prefix = getCurrentLanguage(this.props);
     return (
       <Html lang={prefix}>
         <Head />
