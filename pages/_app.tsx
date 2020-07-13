ba90f846781c1514cf50efebcbf7f6de9f4f7607
Omar Ryhan <omarryhan@gmail.com>
2020-07-13 18:58:35 +0200

    Now changing the html language attribute in _app.tsx page as well to handle client-side transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import Head from 'next/head';
 import App, { AppInitialProps } from 'next/app';
 import '../components/global-styles.css';
 import i18nConfig from '../i18n.config';
+import { changeDocumentLanguage } from '../utils/i18n';
 
 const { allLanguages, defaultLanguage } = i18nConfig;
 
 class MyApp extends App<AppInitialProps> {
+  // change the language again in case it was a client-
+  // side transition (_document.tsx only runs on the server)
+  componentDidMount(): void {
+    const {
+      pageProps,
+    } = this.props;
+
+    const { language } = pageProps;
+    const languageObject = allLanguages[language as string];
+    if (languageObject) {
+      changeDocumentLanguage(languageObject.prefix);
+    }
+  }
+
   public render(): React.ReactElement {
     const {
       Component, pageProps,
